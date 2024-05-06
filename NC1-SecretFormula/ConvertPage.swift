90244f26b64635f94e8b385394ffeeff2deee315
LisandraN08 <lnicoline@student.ciputra.ac.id>
2024-05-06 08:36:53 +0700

    final

--- NC1-SecretFormula/ConvertPage.swift
+++ NC1-SecretFormula/ConvertPage.swift
@@ -5,14 +5,83 @@
 //  Created by Lisandra Nicoline on 27/04/24.
 //
 
+
 import SwiftUI
 
 struct ConvertPage: View {
+    let convertedText: String // Property to receive converted text
+    @State private var isShareSheetPresented = false
+
     var body: some View {
-        Text("Convert Page") 
+        VStack{
+            HStack{
+                Spacer()
+            }
+            VStack {
+                Text("CONVERTED")
+                    .fontWeight(.bold)
+                    .tracking(2.24)
+                    .foregroundColor(.black)
+                    .font(.largeTitle)
+                    .padding(.top, 10)
+                Text("RECIPE")
+                    .fontWeight(.bold)
+                    .tracking(2.24)
+                    .foregroundColor(.black)
+                    .font(.largeTitle)
+                    .padding(.bottom,35)
+                
+            }
+            Spacer()
+            ScrollView {
+                Text(convertedText)
+            }
+            
+            
+            Button(action: {
+                isShareSheetPresented.toggle()
+            }) {
+                Rectangle()
+                    .fill(Color(hex: "AF8260"))
+                    .frame(width: 280, height: 58) // Adjust size as needed
+                    .overlay(
+                        Text("SHARE")
+                            .font(.custom("Akshar", size: 18))
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                    )
+                    .cornerRadius(20) // Optional: Add corner radius for rounded corners
+            }
+            .padding()
+            .sheet(isPresented: $isShareSheetPresented, content: {
+                ActivityViewController(activityItems: [convertedText])
+            })
+        }
+        .navigationBarBackButtonHidden(true)
+        .background(Color(red: 0.992, green: 0.957, blue: 0.890))
     }
 }
 
-#Preview {
-    ConvertPage()
+struct ConvertPage_Previews: PreviewProvider {
+    static var previews: some View {
+        ConvertPage(convertedText: "Sample Converted Text") // Provide a sample converted text
+    }
 }
+
+struct ActivityViewController: UIViewControllerRepresentable {
+    var activityItems: [Any]
+
+    func makeUIViewController(context: Context) -> UIActivityViewController {
+        let controller = UIActivityViewController(activityItems: activityItems, applicationActivities: nil)
+        return controller
+    }
+
+    func updateUIViewController(_ uiViewController: UIActivityViewController, context: Context) {
+    }
+}
+
+//
+//
+//#Preview {
+//    ConvertPage()
+//}
--- NC1-SecretFormula/EditPage.swift
+++ NC1-SecretFormula/EditPage.swift
@@ -10,27 +10,140 @@ import SwiftUI
 struct EditPage: View {
     @Binding var scannedText: String // Define scannedText as a binding parameter
 
+    private func convertText(_ text: String) -> String {
+        var convertedText = ""
+        let lines = text.components(separatedBy: "\n") // Split text into lines
+        
+        for line in lines {
+            if let cupRange = line.range(of: "cup") {
+                // Extract number before "cup"
+                let lineComponents = line.components(separatedBy: " ")
+                for (index, component) in lineComponents.enumerated() {
+                    if let number = parseNumber(from: component) {
+                        let unitIndex = index + 1
+                        if unitIndex < lineComponents.count && lineComponents[unitIndex] == "cup" {
+                            var convertedValue = Double(number)
+                            if line.contains("flour") {
+                                convertedValue = Double(number * 125)
+                            } else if line.contains("bread flour") {
+                                convertedValue = Double(number * 136)
+                            } else if line.contains("sugar") {
+                                convertedValue = Double(number * 200)
+                            } else if line.contains("cocoa powder") {
+                                convertedValue = Double(number * 85)
+                            } else if line.contains("rolled oats") {
+                                convertedValue = Double(number * 85)
+                            } else if line.contains("rice") {
+                                convertedValue = Double(number * 185)
+                            } else if line.contains("honey") {
+                                convertedValue = Double(number * 340)
+                            } else if line.contains("molasses") {
+                                convertedValue = Double(number * 340)
+                            } else if line.contains("syrup") {
+                                convertedValue = Double(number * 340)
+                            } else if line.contains("water") {
+                                convertedValue = Double(number * 237)
+                            } else if line.contains("milk") {
+                                convertedValue = Double(number * 249)
+                            } else if line.contains("butter") {
+                                convertedValue = Double(number * 227)
+                            }
+                            
+                            convertedText += "\(convertedValue) g " + lineComponents.suffix(from: unitIndex + 1).joined(separator: " ")
+                            break
+                        }
+                    }
+                }
+            } else if let ozRange = line.range(of: "oz") {
+                // Extract number before "oz" and multiply by 28.35
+                let lineComponents = line.components(separatedBy: " ")
+                for (index, component) in lineComponents.enumerated() {
+                    if let number = parseNumber(from: component) {
+                        let unitIndex = index + 1
+                        if unitIndex < lineComponents.count && lineComponents[unitIndex] == "oz" {
+                            let convertedValue = Double(number * 28.35)
+                            convertedText += "\(convertedValue) g " + lineComponents.suffix(from: unitIndex + 1).joined(separator: " ")
+                            break
+                        }
+                    }
+                }
+            } else {
+                // If "cup" is not found in the line, keep the original line
+                convertedText += line
+            }
+            convertedText += "\n"
+        }
+        
+        return convertedText
+    }
+
+    private func parseNumber(from string: String) -> Double? {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        if let number = numberFormatter.number(from: string)?.doubleValue {
+            return number
+        } else {
+            let fractionComponents = string.components(separatedBy: "/")
+            if fractionComponents.count == 2,
+               let numerator = Double(fractionComponents[0]),
+               let denominator = Double(fractionComponents[1]),
+               denominator != 0 {
+                return numerator / denominator
+            }
+        }
+        return nil
+    }
+
     var body: some View {
         VStack {
+            Spacer()
+            HStack {
+                Spacer()
+                    Text("SCANNED RECIPE")
+                        .fontWeight(.bold)
+                        .tracking(2.24)
+                        .foregroundColor(.black)
+                        .multilineTextAlignment(.center)
+                        .font(.largeTitle)
+                        .padding(.top, 35)
+                        .padding(.bottom,8)
+                    Spacer()
+
+            }
+            HStack{
+                Text("")
+                    .padding(.leading, 38)
+                Text("Feel free to tweak any errors in your scanned recipes, ensuring culinary accuracy with just a few taps!")
+                    .font(Font.custom("SF", size: 13))
+                    .tracking(1.04)
+                    .foregroundColor(.black)
+                    .multilineTextAlignment(.center)
+                Text("")
+                    .padding(.trailing, 38)
+            }
+            .padding(.bottom, 8)
+
             ScrollView {
                 VStack {
                     ZStack {
                         Color(red: 0.992, green: 0.957, blue: 0.890) // Background color
                             .ignoresSafeArea()
-                        
+
                         TextEditor(text: $scannedText) // Allow editing of scanned text
                             .padding()
                             .transparentScrolling()
+                            .border(Color(hex: "AF8260"))
                             .background(Color(red: 0.992, green: 0.957, blue: 0.890))
                     }
                 }
-                .frame(minHeight: 500)
-                .frame(maxHeight: .infinity) // Fill available space
-                
+                .frame(minHeight: 400)
+                .frame(maxHeight: .infinity)
+                .frame(maxWidth: 300)
+
                 Spacer() // Pushes everything above the button to the top
             }
-            
-            NavigationLink(destination: ConvertPage()) {
+
+            NavigationLink(destination: ConvertPage(convertedText: convertText(scannedText))) {
                 Rectangle()
                     .fill(Color(hex: "AF8260"))
                     .frame(width: 280, height: 58) // Adjust size as needed
@@ -49,6 +162,7 @@ struct EditPage: View {
         .background(Color(red: 0.992, green: 0.957, blue: 0.890))
     }
 }
+
 
 public extension View {
     func transparentScrolling() -> some View {
